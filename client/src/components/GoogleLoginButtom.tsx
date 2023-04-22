bb8ee2c9e4efd4a5836ba0136a3706c05d6bbc60
Juan Mosquella <jmosquella11@gmail.com>
2023-04-22 00:57:07 -0300

    mejorado codigo de auth de google

--- client/src/components/GoogleLoginButtom.tsx
+++ client/src/components/GoogleLoginButtom.tsx
@@ -1,58 +1,78 @@
-/* eslint-disable no-undef */ // Ignorar el error de que "google" no está definido
-// Aquí puedes colocar el código que accede a la API de Google
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setCredentials } from "../redux/slices/auth.slice";
+import { GoogleLogin } from "@react-oauth/google";
+
+declare const window: Window;
 
 const GoogleLoginButtom = () => {
   const dispatch = useDispatch();
-  function handleCallbackResponse(response: any) {
-    const body = { id_token: response.credential };
-    fetch("http://localhost:5000/api/auth/google", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(body),
-    })
-      .then((response) => response.json())
-      .then((response) => {
-        console.log(response);
-        const { token } = response;
-        dispatch(setCredentials(response));
-      })
+  // function handleCallbackResponse(response: any) {
+  //   const body = { id_token: response.credential };
+  //   fetch("http://localhost:5000/api/auth/google", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(body),
+  //   })
+  //     .then((response) => response.json())
+  //     .then((response) => {
+  //       console.log(response);
+  //       const { token } = response;
+  //       dispatch(setCredentials(response));
+  //     })
 
-      .catch((err) => console.warn(err));
-  }
+  //     .catch((err) => console.warn(err));
+  // }
 
-  const logoutGoogle = () => {
-    google.accounts.id.disableAutoSelect();
+  // const logoutGoogle = () => {
+  //   google.accounts.id.disableAutoSelect();
 
-    google.accounts.id.revoke(localStorage.getItem("email"), () => {
-      localStorage.clear();
-      location.reload();
-    });
-  };
+  //   google.accounts.id.revoke(localStorage.getItem("email"), () => {
+  //     localStorage.clear();
+  //     location.reload();
+  //   });
+  // };
 
-  useEffect(() => {
-    google.accounts.id.initialize({
-      client_id:
-        "525882776971-5d7vpp4ptsl894bvp8mer96mr3sv64j8.apps.googleusercontent.com",
-      callback: handleCallbackResponse,
-    });
+  // useEffect(() => {
+  //   google.accounts.id.initialize({
+  //     client_id:
+  //       "525882776971-5d7vpp4ptsl894bvp8mer96mr3sv64j8.apps.googleusercontent.com",
+  //     callback: handleCallbackResponse,
+  //   });
 
-    google.accounts.id.renderButton(document.getElementById("signIn"), {
-      theme: "outline",
-      size: "large",
-    });
-  }, []);
+  //   google.accounts.id.renderButton(document.getElementById("signIn"), {
+  //     theme: "outline",
+  //     size: "large",
+  //   });
+  // }, []);
   return (
-    <button>
-      <div className="w-64" id="signIn"></div>
-      <div className="w-64" id="signOut" onClick={logoutGoogle}>
-        Logout
-      </div>
-    </button>
+    <GoogleLogin
+      onSuccess={(response) => {
+        console.log(response.credential);
+        const body = { id_token: response.credential };
+
+        fetch("http://localhost:5000/api/auth/google", {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify(body),
+        })
+          .then((response) => response.json())
+          .then((response) => {
+            console.log(response);
+            const { token } = response;
+            dispatch(setCredentials(response));
+          })
+
+          .catch((err) => console.warn(err));
+      }}
+      onError={() => {
+        console.log("Login Failed");
+      }}
+    />
   );
 };
 export default GoogleLoginButtom;
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,20 +9,23 @@ import Login from "./pages/Login";
 import { store } from "./redux/store";
 import { Provider } from "react-redux";
 import Register from "./pages/Register";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <BrowserRouter>
-    <Provider store={store}>
-      <Routes>
-        <Route path="/" element={<App />}>
-          <Route element={<ProtectedRoute />}>
-            <Route index element={<Chat />} />
-          </Route>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_CLIENT_ID}>
+      <Provider store={store}>
+        <Routes>
+          <Route path="/" element={<App />}>
+            <Route element={<ProtectedRoute />}>
+              <Route index element={<Chat />} />
+            </Route>
 
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-        </Route>
-      </Routes>
-    </Provider>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+          </Route>
+        </Routes>
+      </Provider>
+    </GoogleOAuthProvider>
   </BrowserRouter>
 );
