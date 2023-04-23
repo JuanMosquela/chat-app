eac10acb1ff1732f720dd7bf0e412f161f70ee2f
Juan Mosquella <jmosquella11@gmail.com>
2023-04-23 16:49:40 -0300

    logica de auth con google temrinada

--- client/src/pages/Chat.tsx
+++ client/src/pages/Chat.tsx
@@ -1,7 +1,7 @@
 import { googleLogout } from "@react-oauth/google";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import io from "socket.io-client";
-import { selectAuth } from "../redux/slices/auth.slice";
+import { logout, selectAuth } from "../redux/slices/auth.slice";
 import { useEffect } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 console.log(import.meta.env.DEV);
@@ -20,26 +20,46 @@ socket.on("disconnect", () => {
   console.log("Desconectado del servidor de Socket.IO");
 });
 
-const logOut = () => {
-  googleLogout();
-  localStorage.clear();
-};
-
 const Chat = () => {
+  const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
   const from = location.state?.from?.pathname || "/";
 
   const auth = useSelector(selectAuth);
 
-  useEffect(() => {
+  const logOut = () => {
+    googleLogout();
+    localStorage.clear();
+    navigate("/login");
+    dispatch(logout());
+
     console.log(auth);
-    if (auth.token) navigate(from);
-  }, [auth]);
+  };
 
   return (
-    <div>
-      <h1>Chat</h1>
+    <div className="p-4">
+      {auth.token && (
+        // <div>
+        //   <img
+        //     className="w-[400px] h-[400px object-cover "
+        //     src={auth.user.picture}
+        //     alt={`${auth.user.username} picture`}
+        //   />
+        //   <h1>{auth.user.username}</h1>
+        //   <span>{auth.user.email}</span>
+        // </div>
+        <div>
+          <img
+            className="rounded-full w-[200px]"
+            src={auth.picture}
+            alt={`${auth.username} profile picture`}
+          />
+          <h1>{auth.username}</h1>
+          <h2>{auth.email}</h2>
+        </div>
+      )}
+      <button onClick={() => logOut()}>Sign Out</button>
     </div>
   );
 };
--- client/src/redux/slices/auth.slice.ts
+++ client/src/redux/slices/auth.slice.ts
@@ -2,13 +2,21 @@ import { createSlice } from "@reduxjs/toolkit";
 import { RootState } from "../store";
 
 interface AuthState {
-  user: string;
+  username: any;
+  picture: string;
+  email: string;
   token: string;
 }
 
 const initialState: AuthState = {
-  user: localStorage.getItem("user")
-    ? JSON.parse(localStorage.getItem("user") || "")
+  username: localStorage.getItem("username")
+    ? JSON.parse(localStorage.getItem("username") || "")
+    : "",
+  picture: localStorage.getItem("picture")
+    ? JSON.parse(localStorage.getItem("picture") || "")
+    : "",
+  email: localStorage.getItem("email")
+    ? JSON.parse(localStorage.getItem("email") || "")
     : "",
   token: localStorage.getItem("token")
     ? JSON.parse(localStorage.getItem("token") || "")
@@ -21,15 +29,21 @@ const authSlice = createSlice({
   reducers: {
     setCredentials: (state, action) => {
       const { user, token } = action.payload;
-      state.user = user;
+      state.username = user.username;
+      state.picture = user.picture;
+      state.email = user.email;
       state.token = token;
 
-      localStorage.setItem("user", JSON.stringify(state.user));
+      localStorage.setItem("user", JSON.stringify(state.username));
+      localStorage.setItem("email", JSON.stringify(state.email));
+      localStorage.setItem("picture", JSON.stringify(state.picture));
       localStorage.setItem("token", JSON.stringify(state.token));
     },
 
     logout: (state) => {
-      state.user = "";
+      state.username = "";
+      state.picture = "";
+      state.email = "";
       state.token = "";
       localStorage.clear();
     },
