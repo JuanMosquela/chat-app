d5494c094e5170fb6395e2175dd3ba2252a902d8
Juan Mosquella <jmosquella11@gmail.com>
2023-05-02 23:40:08 -0300

    prueba de produccion

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,11 @@ import { useEffect } from "react";
 import { Outlet } from "react-router-dom";
 
 function App() {
+  useEffect(() => {
+    import.meta.env.DEV
+      ? console.log("estoy en desarrollo")
+      : console.log("estopy en produccion");
+  }, []);
   return (
     <div>
       <Outlet />
